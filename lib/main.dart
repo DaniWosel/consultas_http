1bb3885bcea5ddd9f2e15796f9f3b979fdfcd951
DaniWosel <daniwosel@gmail.com>
2020-10-21 17:41:55 +0200

    medio arreglado muestra contenido pagina http

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'respuesta.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-
-  var response;
-
-  Future<String> consultar() async{
-
-    print("lanza la consulta");
-    var respuesta = await http.get('https://jsonplaceholder.typicode.com/posts/1');
-
-    // sample info available in response
-    int statusCode = respuesta.statusCode;
-    Map<String, String> headers = respuesta.headers;
-    String contentType = headers['content-type'];
-    String js = respuesta.body;
-    print(statusCode.toString()+"\n"+ headers.toString()+"\n"+ js.toString());
-    print(respuesta.toString());
-    return js;
-  }
-
-
-  MyApp(){
-    print("constructor");
-    var s = consultar().then((x){
-      setState((){
-        response=x;
-      });
-    });
-    print (s.toString());
-  }
-
-  class LoginState extends State<Login>{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Flutter layout demo'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Respuesta(),
         ),
       ),
     );
   }
 }
+
+
 
