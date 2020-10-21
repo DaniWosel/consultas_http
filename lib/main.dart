35c0f209ecbdb631dec0100c82874bf60e10f982
DaniWosel <daniwosel@gmail.com>
2020-10-21 13:57:50 +0200

    cosas mal

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
+import 'package:http/http.dart' as http;
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  var response;
+
+  Future<String> consultar() async{
+
+    print("lanza la consulta");
+    var respuesta = await http.get('https://jsonplaceholder.typicode.com/posts/1');
+
+    // sample info available in response
+    int statusCode = respuesta.statusCode;
+    Map<String, String> headers = respuesta.headers;
+    String contentType = headers['content-type'];
+    String js = respuesta.body;
+    print(statusCode.toString()+"\n"+ headers.toString()+"\n"+ js.toString());
+    print(respuesta.toString());
+    return js;
+  }
+
+
+  MyApp(){
+    print("constructor");
+    var s = consultar().then((x){
+      setState((){
+        response=x;
+      });
+    });
+    print (s.toString());
+  }
+
+  class LoginState extends State<Login>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
